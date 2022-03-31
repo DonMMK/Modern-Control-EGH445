d80404907ec31b86a780e0f3e497fb093ad7732a
don <misurakalu@gmail.com>
2022-03-31 12:52:53 +1000

    Update Main.m

--- Practicals/Week 4/Main.m
+++ Practicals/Week 4/Main.m
@@ -4,7 +4,7 @@
     m  = 0.15;% kg
     lm = 0.2; % m
     gm = 9.81;% m/s^2
-Model_Constants = [Mc , m , lm , g]
+Model_Constants = [Mc , m , lm , gm];
 
 
 % Initial Condition
@@ -19,19 +19,32 @@ x0 = x0_a;
 % Pass the parameters to the matrices
 Aa = [0 , 0 , 1 , 0; 
      0 , 0, 0 , 1;
-     0 , (-g*m)/Mc , 0 , 0];
-
-Ab = [];
+     0 , (-gm*m)/Mc , 0 , 0];
 
 Ba = [0 , 0 , 1/Mc , -1/(lm * Mc)];
 
 
-Bb = [];
 
-C;
+Ca = eye(2 ,4);
 
-D;
+Da = zeros(2,1);
 
+Ab = [0 , 0 , 1, 0;
+      0 , 0 , 0 ,1;
+      0 , -gm/Mc , 0 , 0;
+      0 , (-gm(Mc + m))/(lm * Mc) , 0 , 0];
+
+Bb = [0 ;
+      0 ;
+      inv(Mc);
+      inv(lm * Mc)];
+
+
+
+
+
+xa_bar = [0 , 0 ,0 ,0 ]';
+x0_a = [0.2 , (20*pi)/180 , 0 , 0]';
 
 
 
